727d5eeb7534e5035859112e97a68550f60a105e
CpuKnight <CpuKnight@CPUKNIGHT-PC>
2017-07-27 14:38:25 +0800

    Migrated Over to Login Template

---
+++ SSD-Migrated/src/SSD-Migrated/Services/IMessageRespository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SSD_Migrated.Models.MessageModels;
+
+namespace SSD_Migrated.Services
+{
+    public interface IMessageRespository
+    {
+        IEnumerable<Message> Messages { get; }
+    }
+}

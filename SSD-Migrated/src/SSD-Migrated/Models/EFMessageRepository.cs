d9c90612d72afd41963a65beecdb72b37af9e48d
Aqil <trektrace178@gmail.com>
2017-07-29 22:21:51 +0800

    Database Connections for Identity

---
+++ SSD-Migrated/src/SSD-Migrated/Models/EFMessageRepository.cs
@@ -0,0 +1,20 @@
+﻿using SSD_Migrated.Data;
+using SSD_Migrated.Services;
+using System.Collections.Generic;
+
+
+namespace SSD_Migrated.Models
+{
+    public class EFMessageRepository : IMessageRespository
+    {
+        private AppThreadDbContext context;
+
+        public EFMessageRepository(AppThreadDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IEnumerable<MessageModels.Message> Messages => context.Messages;
+    }
+   
+}
--- SSD-Migrated/src/SSD-Migrated/Startup.cs
+++ SSD-Migrated/src/SSD-Migrated/Startup.cs
@@ -42,7 +42,7 @@ namespace SSD_Migrated
         {
             // Add framework services.
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("AppIdentity")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
